803b82290afdc57b306714054f2d76c689fc9483
Lauro Mendes do Amaral Junior <lauro.juniorspx@gmail.com>
2020-07-03 02:10:54 -0700

    Criando ambiente virtual

--- Aula_flask/app.py
+++ Aula_flask/app.py
@@ -10,3 +10,9 @@ def index():
 
 if __name__ == '__main__':
     app.run()
+
+
+# Vamos usar a estrutura de organização MVC:
+# Model (Banco de dados- informações)
+# View  (Tudo aquilo que o usuário vê. Desde a página HTML, a estilos CSSs e JS(arquivos estáticos))
+# Controller (Parte lógica. Onde gravar, onde editar, onde deletar... etc)
