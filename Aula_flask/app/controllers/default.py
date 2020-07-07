e588f6aca3334111605cd7149a836a8db0f42a00
Lauro Mendes do Amaral Junior <lauro.juniorspx@gmail.com>
2020-07-06 23:08:30 -0700

    Inclusão de novas rotas/

--- Aula_flask/app/controllers/default.py
+++ Aula_flask/app/controllers/default.py
@@ -1,9 +1,19 @@
 from app import app
+from flask import render_template
 
 
 @app.route('/')
 def index():
+    lista = ['Daniel', 'Marcio', 'Lauro']
+    return render_template('index.html', lista=lista)
+
+@app.route('/ola-mundo')
+def ola_mundo():
     return 'Hello, World!'
+
+@app.route('/login')
+def login():
+    return 'Aqui será feito seu login'
 
 # Aqui criamos a parte lógica, daquilo que tem a finalidade de mostrar ao usuário aquilo que
 # queremos mostrar á ele.
